62df8f9cb7ec406f2e3f24b76f7540e87548a24c
Panhara28 <titpanhra@gmail.com>
2017-08-02 16:19:18 +0700

    Add model user and Do some validation and add index to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  # Name has to presence, Name Length has to be 50
+  validates :name, presence: true, length: { maximum: 50 }
+  #Constant Variable With REGEX
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #Email has to be presence and length should be 255 if not failed to save and also email should unique
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
 end
