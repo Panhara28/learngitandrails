0daaceb8ce195180c67ce662526bad09d76477ff
Panhara28 <titpanhra@gmail.com>
2017-08-02 16:50:47 +0700

    creating user authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
   #validate password to be presence and minimum of the password is 6
-  validates :password, presence: true, :length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 end
