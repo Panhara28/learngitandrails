b5a6c344b206ccc9773bd96ec27f5341587b31c4
Panhara28 <titpanhra@gmail.com>
2017-08-02 14:06:29 +0700

    Add partialize file and add some pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homes#index"
+  get "about"=>"pages#about"
+  get "help"=>"pages#help"
+  get "contact"=>"pages#contact"
+  get "login"=>"pages#login"
 end
