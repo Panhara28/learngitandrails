7d85fba0201cf18fc9475e10977e699676b77179
Panhara28 <titpanhra@gmail.com>
2017-08-02 16:29:44 +0700

    Add gem bcrypt and add password_digest to users and validate a password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,8 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   #Email has to be presence and length should be 255 if not failed to save and also email should unique
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
-            uniqueness: true
+            uniqueness: { case_sensitive: false }
+  #validate password to be presence and minimum of the password is 6
+  validates :password, presence: true, :length: { minimum: 6 }
+  has_secure_password
 end
