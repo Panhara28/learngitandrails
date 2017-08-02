09240a30e75b20fcd33b4b6c5f50bb6dc04acd2a
Panhara28 <titpanhra@gmail.com>
2017-08-02 14:18:01 +0700

    add controller user to create user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homes#index"
   get "about"=>"pages#about"
   get "help"=>"pages#help"
   get "contact"=>"pages#contact"
   get "login"=>"pages#login"
+  get "signup"=>"users#new"
 end
