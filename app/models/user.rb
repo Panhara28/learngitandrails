061242ece1871e9822a3cba4059935489ca1fa96
Panhara28 <titpanhra@gmail.com>
2017-08-02 15:51:57 +0700

    Add PWd to db
    Add model user and Do some validation and add index to user
    
    Add gem bcrypt and add password_digest to users and validate a password
    
    creating user authenticate

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  # Name has to presence, Name Length has to be 50
+  validates :name, presence: true, length: { maximum: 50 }
+  #Constant Variable With REGEX
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #Email has to be presence and length should be 255 if not failed to save and also email should unique
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  #validate password to be presence and minimum of the password is 6
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
+end
