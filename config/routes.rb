2cbeac29824039cfd2fed88870a93ecc110890d3
Panhara28 <titpanhra@gmail.com>
2017-08-04 12:55:14 +0700

    add session

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homes#index"
   get "about"=>"pages#about"
   get "help"=>"pages#help"
   get "contact"=>"pages#contact"
-  get "login"=>"pages#login"
+  get "login"=>"sessions#new"
+  post 'login'=>"sessions#create"
+  delete 'logout'=>"sessions#destroy"
   get "signup"=>"users#new"
 
 end
